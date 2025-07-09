c5650aff0cf1f21b08bbfbaf0f5b00fe9158d429
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-07-09 14:50:59 -0400

    Initial Commit

---
+++ ThemeSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThemeSwitcher
+//
+//  Created by RajayGoms on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThemeSwitcher/ThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitcherApp.swift
+//  ThemeSwitcher
+//
+//  Created by RajayGoms on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
